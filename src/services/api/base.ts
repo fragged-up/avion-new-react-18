c65a4fc2ef12be1542bfa8e289514a066ece3501
fragged-up <154810298+fragged-up@users.noreply.github.com>
2025-04-20 02:22:39 +0300

    catalog api

--- src/services/api/base.ts
+++ src/services/api/base.ts
@@ -1,10 +1,6 @@
-import DandyChair from '@/assets/images/dandy-desktop.svg';
-import LucyLamp from '@/assets/images/lucy-desktop.svg';
-import RusticVase from '@/assets/images/rustic-desktop.svg';
-import SilkyVase from '@/assets/images/silky-desktop.svg';
-import PopularRastic from '@/assets/images/popular-rustic.svg';
-import PopularLucy from '@/assets/images/popular-lucylamp.svg';
-
+import { DandyChair, LucyLamp, PopularLucy, PopularRastic, RusticVase, SilkyVase } from "@/services/api/catalog"
+import { dDandy, dRustic, dSilky, dLucy, dFlights, dVase, dSm, dOrangeLucy } from "@/services/api/catalog"
+import { LucyCatalog,DandyCatalog,RusticCatalog,SilkyCatalog } from "@/services/api/catalog"
 export const CatalogAPI = [
   { id: 1, image: DandyChair, title: 'The Dandy Chair', price: 250,},
   { id: 2,image: RusticVase,title: 'Rustic Vase Set',price: 155, },
@@ -19,3 +15,17 @@ export const OurPopAPI = [
   {id:3, image:PopularLucy,title:'The Lucy Lamp',price:399,},
   {id:4, image:PopularRastic,title:'The Lucy Lamp',price:155,},
 ]
+
+// export const BaseProducts = [
+//   {image:, title:, price: },
+//   {image:, title:, price: },
+//   {image:, title:, price: },
+//   {image:, title:, price: },
+//   {image:, title:, price: },
+//   {image:, title:, price: },
+//   {image:, title:, price: },
+//   {image:, title:, price: },
+//   {image:, title:, price: },
+//   {image:, title:, price: },
+//   {image:, title:, price: },
+// ]
---
+++ src/services/api/catalog/images.ts
@@ -0,0 +1,50 @@
+
+import DandyChair from '@/assets/images/dandy-desktop.svg';
+import LucyLamp from '@/assets/images/lucy-desktop.svg';
+import RusticVase from '@/assets/images/rustic-desktop.svg';
+import SilkyVase from '@/assets/images/silky-desktop.svg';
+import PopularRastic from '@/assets/images/popular-rustic.svg';
+import PopularLucy from '@/assets/images/popular-lucylamp.svg';
+
+
+import LucyCatalog from "@/assets/images/lucy-catalog.svg";
+import SilkyCatalog from "@/assets/images/silky-catalog.svg";
+import RusticCatalog from "@/assets/images/rustic-catalog.svg";
+import DandyCatalog from "@/assets/images/dandy-catalog.svg";
+
+import dDandy from "@/assets/deskProduct/DANDY.svg";
+import dRustic from "@/assets/deskProduct/RUSTIC.svg";
+import dSilky from "@/assets/deskProduct/SILKY.svg";
+import dLucy from "@/assets/deskProduct/LUCY.svg";
+import dFlights from "@/assets/deskProduct/FLIGHTS.svg";
+import dVase from "@/assets/deskProduct/VASE.svg";
+import dSm from "@/assets/deskProduct/SMALLCHR.svg";
+import dOrangeLucy from "@/assets/deskProduct/ORANGELUCY.svg";
+
+
+export {
+  DandyChair,
+  LucyLamp,
+  PopularLucy,
+  PopularRastic,
+  RusticVase,
+  SilkyVase,
+
+
+  LucyCatalog,
+  DandyCatalog,
+  RusticCatalog,
+  SilkyCatalog,
+
+
+  dDandy,
+  dRustic,
+  dSilky,
+  dLucy,
+  dFlights,
+  dVase,
+  dSm,
+  dOrangeLucy,
+
+
+}
